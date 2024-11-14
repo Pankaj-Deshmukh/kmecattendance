c3afa908fc9e2d5ef89a31dbbf7188edecdde105
Pankaj-Deshmukh <pankajdeshmukh2002@gmail.com>
2024-11-14 13:57:25 +0530

    added name

--- app/page.tsx
+++ app/page.tsx
@@ -39,9 +39,9 @@ export default function Home() {
       <InputBox />
       {/* Only render Counter once rollno is available */}
       {rollno && <Counter targetNumber={attendance} duration={1500} rollnumber={rollno} />}
-      {!rollno && <Counter targetNumber={100} duration={1500} rollnumber={"Enter your Roll number"} />}
+      {!rollno && <Counter targetNumber={0} duration={1500} rollnumber={"Enter your Roll number"} />}
       <p className="text-center font-thin font-sans">
-        powered by <strong>KMEC</strong> student.
+        powered by <strong>Pankaj Deshmukh</strong>.
       </p>
     </div>
   );
