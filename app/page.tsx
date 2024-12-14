c45264520ebc604d8b79f5027095694263ab2819
Pankaj-Deshmukh <pankaj.deshmukh.2110@gmail.com>
2024-12-14 15:37:25 +0530

    added dynamic quotes, some info on front end

--- app/page.tsx
+++ app/page.tsx
@@ -22,12 +22,18 @@ interface DayObject {
   holiday: "true" | "false";
   sessions: SessionData;
 }
-
+interface Quote {
+  q: string,
+  a: string,
+  h: string
+}
 
 export default function Home() {
   const [attendance, setAttendance] = useState<number>(0); // Ensure type is a number
   const [rollno, setRollno] = useState<string | null>(null); // State for storing rollno
   const [session, setSession] = useState<DayObject[] | null>(null); // state for storing session data
+  const [quote, setQuote] = useState<Quote | null>(null); // state for storing the generated quote
+  const [isClient, setIsClient] = useState<Boolean>(false);
 
   useEffect(() => {
     // Check for rollno after the component mounts
@@ -55,16 +61,35 @@ export default function Home() {
       };
       fetchAttendance();
     }
+    setIsClient(true);
   }, [rollno]);
+
+  useEffect(()=>{
+    if (isClient) {
+      const getQuote = async () => {
+        try {
+          const res = await axios.get("/api/quote");
+          setQuote(res.data[0]); // Set quote from API response
+          console.log(res.data[0]);
+        } catch (error) {
+          console.error("Error occurred while fetching the quote:", error);
+        }
+      };
+      getQuote();
+    }
+  }, [isClient, rollno]);
 
   return (
     <div className="flex flex-col w-screen h-screen bg-gray-50">
+      <h1 className="text-center font-bold font-sans mt-2">Welcome to Keshav Memorial Engineering College Attendance Tracker!</h1>
+      <p className="text-center mb-2 font-light text-xs">See your attendance anytime, anywhere – right at your fingertips.</p>
       <InputBox />
       {/* Only render Counter once rollno is available */}
       {rollno && <AttendanceTable data={session}/>}
       {rollno && <Counter targetNumber={attendance} duration={1500} rollnumber={rollno} />}
       {!rollno && <Counter targetNumber={0} duration={1500} rollnumber={"Enter your Roll number."} />}
-      <p className="text-center font-thin font-sans">
+      {quote && <p className="text-center text-lg font-semibold mb-3">"{quote.q}" -- <span className="font-light">{quote.a}</span></p>}
+      <p className="text-center font-thin font-sans mt-5">
         <a href="https://www.linkedin.com/in/pankaj-deshmukh-142573329/">
           powered by <strong className="font-semibold text-red-600">Pankaj Deshmukh</strong>.
         </a>
