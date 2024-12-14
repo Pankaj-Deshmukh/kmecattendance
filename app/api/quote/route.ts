a6832db154f17db6568b152cfaa60b693422a57c
Pankaj-Deshmukh <pankaj.deshmukh.2110@gmail.com>
2024-12-14 15:49:16 +0530

    added dynamic quotes, some info on front end

--- app/api/quote/route.ts
+++ app/api/quote/route.ts
@@ -5,7 +5,7 @@ export async function GET(){
     try {
         const response = await axios.get('https://zenquotes.io/api/random');
         return NextResponse.json(response.data); // Send the quote data to the frontend
-      } catch (error) {
+      } catch () {
         return NextResponse.json({status:500},{statusText:'Error fetching quote'});
       }
 }
