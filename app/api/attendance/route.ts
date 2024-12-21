f942ab8138882cf728fd8f0cc049d58a857cd508
Pankaj-Deshmukh <pankaj.deshmukh.2110@gmail.com>
2024-12-21 12:47:56 +0530

    added 1st year csm

--- app/api/attendance/route.ts
+++ app/api/attendance/route.ts
@@ -64,7 +64,7 @@ export async function GET(req:Request){
             rollno = 2023071 + Number(orgRollno?.substring(9,12));
         }
         else if(Number(orgRollno?.substring(6,9))===748){
-            rollno = 2022944 + Number(orgRollno?.substring(9,12));
+            rollno = 2022943 + Number(orgRollno?.substring(9,12));
         }
         else{
             console.error("Enter a valid Roll number.");
